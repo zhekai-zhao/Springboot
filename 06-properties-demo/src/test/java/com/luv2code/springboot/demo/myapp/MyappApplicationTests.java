b2104d8303167fbe3ce221453af638495fc9286b
zhekai-zhao <zhekai1@ualberta.ca>
2023-07-19 16:55:30 -0600

    propertites

---
+++ 06-properties-demo/src/test/java/com/luv2code/springboot/demo/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
